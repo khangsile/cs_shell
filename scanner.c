6c717443240000fce6cf5b9f5b58bee6be8029b6
Khang Le <khangsile@gmail.com>
2014-04-05 00:02:51 -0400

    fix compile errors in scanner.c, token.c

--- scanner.c
+++ scanner.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 #include "variables.h"
+#include "token.h"
 
 extern int yylex();
 extern int yylineno;
+extern int yyleng;
 extern char* yytext;
+
+char* cpystr(char*);
+
+struct token *head = NULL;
 
 int main(void) {
 
   int ntoken, vtoken;
 
+  // Get head first.
   ntoken = yylex();
   while(ntoken) {
-    if (ntoken != NEWLINE)
-      printf("%d: %s\n", ntoken, yytext);
+    struct token *tk = (struct token*) malloc(sizeof(struct token));
+    tk->type = ntoken;
+    tk->text = cpystr(yytext);
+    if (tk->type != 4)
+      printf("%s\n", tk->text);
     ntoken = yylex();
   }
 
   return 0;
+}
+
+char* cpystr(char* str) {
+  int size = strlen(str);
+  char *cpy = (char*) malloc(size);
+  strcpy(cpy, str);
+  return cpy;
 }
